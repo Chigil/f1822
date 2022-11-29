b9dd40ade50d2e5e757ad427d4ecc27f140a5dfd
Chigileychik Andrey <chigileychik_a@itstep.lan>
2022-11-29 22:03:27 +0300

    extend Post component for User

--- src/pages/Posts.tsx
+++ src/pages/Posts.tsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import http from "../http";
 
-const Posts = () => {
+const Posts = ({ userId = 0 }: { userId?: number }) => {
+    const [posts, setPosts] = useState<any[]>([]);
+    useEffect(() => {
+        getAllPosts();
+    },[]);
+
+    const getAllPosts = async () => {
+        try {
+            const responseData = await http.get(userId === 0 ? '/posts' : `/posts?userId=${userId}`);
+            const posts = responseData.data;
+            setPosts(posts);
+        } catch (err) {
+            alert(err);
+        }
+    };
     return (
         <div>
-            <h1>THIS is Posts page!!!</h1>
+            {userId === 0 ? <h1>THIS is Posts page!!!</h1> : <h3>User posts:</h3>}
+            {posts.map(post =>
+                <div key={post.id}>
+                    {post.body}
+                </div>
+            )}
         </div>
     );
 };
--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -1,22 +1,43 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import http from "../http";
-import { useParams } from 'react-router-dom'
+import {useParams} from 'react-router-dom'
+import {IUser} from "../components/Users/interfaces";
+import Posts from "./Posts";
 
 const User = () => {
+    const [user, setUser] = useState<IUser>({
+        id: 0,
+        name: '',
+        username: '',
+        phone: '',
+        email: '',
+        website: ''
+    });
     const params = useParams();
+    const userId = params.id ? +params.id : 0;
     useEffect(() => {
         getAllUserDataById();
-    },[]);
+    }, []);
     const getAllUserDataById = async () => {
-        const userData = await http.get(`/users/${params.id}`);
-        const userPostsData = await http.get(`/posts?userId=${userData.data.id}`);
+        const userData = await http.get(`/users/${userId}`);
+        setUser(userData.data);
         //get posts comments
-        console.log(userData.data);
-        console.log(userPostsData.data);
     }
+    const updateUserInfo = async () => {
+        const newUser = await http.put(`/users/${userId}`, user);
+        setUser(newUser.data);
+    }
+    console.log(user);
     return (
         <div>
             <h1>I'am single cool User </h1>
+            {Object.keys(user).map(field => {
+                return <input value={user[field as keyof Omit<IUser, 'address' | 'company'>]}
+                              onChange={event => setUser({...user, [field]: event.target.value})}
+                />
+            })}
+            <Posts userId={userId} />
+            <button className="btn btn-success" onClick={() => updateUserInfo()}>Update user info</button>
         </div>
     );
 };
